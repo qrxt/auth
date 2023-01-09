147d6af9aa1fc92c0f46f63311c4113c3ea2c057
qrxt <qrx357@gmail.com>
2023-01-09 05:08:08 +0300

    Add register validation

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,17 @@
+const { validationResult } = require("express-validator");
+const ApiError = require("../exceptions/api-error");
 const userService = require("../service/user-service");
 
 class UserController {
   async register(req, res, next) {
     try {
       const thirtyDays = 30 * 24 * 60 * 60 * 1000;
+
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest("Validation error", errors.array()));
+      }
 
       const { email, password } = req.body;
       const userData = await userService.register(email, password);
--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,14 @@
 const userController = require("../controllers/user-controller");
 const Router = require("express").Router;
+const { body } = require("express-validator");
 const router = new Router();
 
-router.post("/register", userController.register);
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6, max: 32 }),
+  userController.register
+);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.get("/activate/:link", userController.activate);
